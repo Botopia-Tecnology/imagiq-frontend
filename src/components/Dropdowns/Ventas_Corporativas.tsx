c31f5859b39b097813acaa329e859bad90bd753b
Santiago Aristizabal <aristizabalsantiago482@gmail.com>
2025-09-03 16:57:57 -0500

    feat: Remove unused dropdown components for mobile devices, appliances, and televisions

--- src/components/Dropdowns/Ventas_Corporativas.tsx
+++
@@ -1,5 +0,0 @@
-// This file exists to satisfy TypeScript compilation but is not used
-// Dropdowns are only available for: Dispositivos móviles, Televisores y AV, Electrodomésticos
-export default function VentasCorporativasDropdown() {
-  return null;
-}

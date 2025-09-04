57bfdc33cc58e69729e491c83c4dc34add4b586c
User <user@example.com>
2025-09-04 11:20:26 -0500

    Feat: Se mejoran las animaciones al finalizar el pago dentro del carrito, además de que se cambia y se añade el nuevo logo de sumsung

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -287,12 +287,31 @@ export default function Navbar() {
               posthogUtils.capture("logo_click", { source: "navbar" })
             }
             aria-label="Inicio"
+            className="flex items-center gap-2"
           >
+            <Image
+              src={
+                showWhiteLogo ? "/frame_311_white.png" : "/frame_311_black.png"
+              }
+              alt="Q Logo"
+              height={32}
+              width={32}
+              style={{ minWidth: 32, width: 32 }}
+              priority
+            />
             <Image
               src={showWhiteLogo ? logoSamsungWhite : logoSamsungBlack}
               alt="Samsung Logo"
               height={32}
               style={{ minWidth: 120, width: "auto" }}
+              priority
+            />
+            <Image
+              src={showWhiteLogo ? "/store_white.png" : "/store_black.png"}
+              alt="Store Logo"
+              height={20}
+              width={60}
+              style={{ minWidth: 36, width: 36 }}
               priority
             />
           </Link>

d5177376185a80fed88326d6afe76ba867659e07
User <user@example.com>
2025-09-06 23:04:26 -0500

    Feat: Se añade codigo sku a cada producto

--- src/app/productos/data_product/products.ts
+++ src/app/productos/data_product/products.ts
@@ -22,6 +22,7 @@ export const productsData = {
     // Accesorios generales
     {
       id: "cargador-rapido-25w",
+      sku: "cargador-rapido-25w",
       name: "Samsung Cargador Rápido 25W USB-C",
       image: smartphonesImg,
       colors: [
@@ -36,6 +37,7 @@ export const productsData = {
     },
     {
       id: "funda-silicona-s24",
+      sku: "funda-silicona-s24",
       name: "Samsung Funda Silicona Galaxy S24 Ultra",
       image: tabletasImg,
       colors: [
@@ -50,6 +52,7 @@ export const productsData = {
     },
     {
       id: "correa-cuero-watch",
+      sku: "correa-cuero-watch",
       name: "Samsung Correa de Cuero Galaxy Watch",
       image: galaxyWatchImg,
       colors: [
@@ -66,6 +69,7 @@ export const productsData = {
     },
     {
       id: "protector-pantalla-tab",
+      sku: "protector-pantalla-tab",
       name: "Protector de Pantalla Galaxy Tab S9",
       image: tabletasImg,
       colors: [
@@ -78,6 +82,7 @@ export const productsData = {
     // Galaxy Buds
     {
       id: "galaxy-buds2-pro",
+      sku: "galaxy-buds2-pro",
       name: "Samsung Galaxy Buds2 Pro",
       image: galaxyBudsImg,
       colors: [
@@ -94,6 +99,7 @@ export const productsData = {
     },
     {
       id: "galaxy-buds-pro",
+      sku: "galaxy-buds-pro",
       name: "Samsung Galaxy Buds Pro",
       image: galaxyBudsImg,
       colors: [
@@ -109,6 +115,7 @@ export const productsData = {
     },
     {
       id: "galaxy-buds-fe",
+      sku: "galaxy-buds-fe",
       name: "Samsung Galaxy Buds FE",
       image: galaxyBudsImg,
       colors: [
@@ -122,6 +129,7 @@ export const productsData = {
     // Galaxy Watch
     {
       id: "galaxy-watch6-44mm",
+      sku: "galaxy-watch6-44mm",
       name: "Samsung Galaxy Watch 6 44mm",
       image: galaxyWatchImg,
       colors: [
@@ -140,6 +148,7 @@ export const productsData = {
   "tv-monitores-audio": [
     {
       id: "samsung-qled-55",
+      sku: "samsung-qled-55",
       name: "Samsung QLED 55",
       image: tvImg,
       colors: [
@@ -154,6 +163,7 @@ export const productsData = {
     },
     {
       id: "samsung-crystal-50",
+      sku: "samsung-crystal-50",
       name: "Samsung Crystal UHD 50",
       image: tvImg,
       colors: [
@@ -167,6 +177,7 @@ export const productsData = {
     },
     {
       id: "samsung-monitor-smart",
+      sku: "samsung-monitor-smart",
       name: "Samsung Monitor Smart 27",
       image: monitorImg,
       colors: [
@@ -181,6 +192,7 @@ export const productsData = {
     },
     {
       id: "samsung-soundbar-a550",
+      sku: "samsung-soundbar-a550",
       name: "Samsung Soundbar HW-A550",
       image: audioImg,
       colors: [
@@ -197,6 +209,7 @@ export const productsData = {
     // Smartphones
     {
       id: "galaxy-a16",
+      sku: "galaxy-a16",
       name: "Samsung Galaxy A16",
       image: smartphonesImg,
       colors: [
@@ -212,6 +225,7 @@ export const productsData = {
     },
     {
       id: "galaxy-a25",
+      sku: "galaxy-a25",
       name: "Samsung Galaxy A25",
       image: smartphonesImg,
       colors: [
@@ -225,6 +239,7 @@ export const productsData = {
     },
     {
       id: "galaxy-a26",
+      sku: "galaxy-a26",
       name: "Samsung Galaxy A26",
       image: smartphonesImg,
       colors: [
@@ -240,6 +255,7 @@ export const productsData = {
     },
     {
       id: "galaxy-a15-256gb",
+      sku: "galaxy-a15-256gb",
       name: "Samsung Galaxy A15 256 GB",
       image: smartphonesImg,
       colors: [
@@ -252,6 +268,7 @@ export const productsData = {
     },
     {
       id: "galaxy-a15-4gb",
+      sku: "galaxy-a15-4gb",
       name: "Samsung Galaxy A15 4GB 128GB",
       image: smartphonesImg,
       colors: [
@@ -265,6 +282,7 @@ export const productsData = {
     },
     {
       id: "galaxy-a15-128gb",
+      sku: "galaxy-a15-128gb",
       name: "Samsung Galaxy A15 4GB 128GB",
       image: smartphonesImg,
       colors: [
@@ -278,6 +296,7 @@ export const productsData = {
     // Tablets
     {
       id: "galaxy-tab-s9-11",
+      sku: "galaxy-tab-s9-11",
       name: 'Samsung Galaxy Tab S9 11" WiFi',
       image: tabletasImg,
       colors: [
@@ -294,6 +313,7 @@ export const productsData = {
     },
     {
       id: "galaxy-tab-a9-10",
+      sku: "galaxy-tab-a9-10",
       name: 'Samsung Galaxy Tab A9+ 10.4" WiFi',
       image: tabletasImg,
       colors: [
@@ -306,6 +326,7 @@ export const productsData = {
     },
     {
       id: "galaxy-tab-s9-fe",
+      sku: "galaxy-tab-s9-fe",
       name: 'Samsung Galaxy Tab S9 FE 10.9" WiFi',
       image: tabletasImg,
       colors: [
@@ -324,6 +345,7 @@ export const productsData = {
     // Aire Acondicionado
     {
       id: "ar12t93",
+      sku: "ar12t93",
       name: "Samsung Aire Acondicionado Split Inverter 12000 BTU AR12T93",
       image: aireImg,
       colors: [
@@ -339,6 +361,7 @@ export const productsData = {
     },
     {
       id: "ar18t93",
+      sku: "ar18t93",
       name: "Samsung Aire Acondicionado Split Inverter 18000 BTU AR18T93",
       image: aireImg,
       colors: [
@@ -354,6 +377,7 @@ export const productsData = {
     // Aspiradoras
     {
       id: "asp-1",
+      sku: "asp-1",
       name: "Aspiradora Samsung Jet 90",
       image: aspiradoraImg,
       colors: [
@@ -369,6 +393,7 @@ export const productsData = {
     },
     {
       id: "asp-2",
+      sku: "asp-2",
       name: "Aspiradora LG CordZero",
       image: aspiradoraImg,
       colors: [
@@ -383,6 +408,7 @@ export const productsData = {
     },
     {
       id: "asp-3",
+      sku: "asp-3",
       name: "Aspiradora de cilindro Whirlpool",
       image: aspiradoraImg,
       colors: [
@@ -398,6 +424,7 @@ export const productsData = {
     },
     {
       id: "asp-4",
+      sku: "asp-4",
       name: "Robot Aspirador Philips",
       image: aspiradoraImg,
       colors: [
@@ -413,6 +440,7 @@ export const productsData = {
     // Hornos
     {
       id: "horno-1",
+      sku: "horno-1",
       name: "Horno Samsung Digital",
       price: "$299.99",
       image: hornosImg,
@@ -428,6 +456,7 @@ export const productsData = {
     },
     {
       id: "horno-2",
+      sku: "horno-2",
       name: "Horno Whirlpool Convencional",
       price: "$199.99",
       image: hornosImg,
@@ -443,6 +472,7 @@ export const productsData = {
     },
     {
       id: "horno-3",
+      sku: "horno-3",
       name: "Horno LG Microondas",
       price: "$149.99",
       image: hornosImg,
@@ -459,6 +489,7 @@ export const productsData = {
     // Lavadoras
     {
       id: "ww22k6800ew",
+      sku: "ww22k6800ew",
       name: "Samsung Lavadora Carga Frontal 22kg WW22K6800EW",
       image: lavadoraImg,
       colors: [
@@ -474,6 +505,7 @@ export const productsData = {
     },
     {
       id: "wa16t6260bw",
+      sku: "wa16t6260bw",
       name: "Samsung Lavadora Carga Superior 16kg WA16T6260BW",
       image: lavadoraImg,
       colors: [
@@ -489,6 +521,7 @@ export const productsData = {
     // Lavavajillas
     {
       id: "dw60m5052fs",
+      sku: "dw60m5052fs",
       name: "Samsung Lavavajillas Integrable 14 cubiertos DW60M5052FS",
       image: lavavajillasImg,
       colors: [
@@ -504,6 +537,7 @@ export const productsData = {
     },
     {
       id: "dw60m6050fs",
+      sku: "dw60m6050fs",
       name: "Samsung Lavavajillas Libre Instalación 13 cubiertos DW60M6050FS",
       image: lavavajillasImg,
       colors: [
@@ -519,6 +553,7 @@ export const productsData = {
     // Microondas
     {
       id: "ms23k3513aw",
+      sku: "ms23k3513aw",
       name: "Samsung Microondas 23L MS23K3513AW",
       image: microondasImg,
       colors: [
@@ -534,6 +569,7 @@ export const productsData = {
     },
     {
       id: "mg23t5018ak",
+      sku: "mg23t5018ak",
       name: "Samsung Microondas Grill 23L MG23T5018AK",
       image: microondasImg,
       colors: [
@@ -549,6 +585,7 @@ export const productsData = {
     // Refrigeradores
     {
       id: "rf28r7351sr",
+      sku: "rf28r7351sr",
       name: "Samsung Refrigerador French Door 27.4 pies RF28R7351SR",
       image: refrigeradorImg,
       colors: [
@@ -563,6 +600,7 @@ export const productsData = {
     },
     {
       id: "rs27t5200s9",
+      sku: "rs27t5200s9",
       name: "Samsung Refrigerador Side by Side 27 pies RS27T5200S9",
       image: refrigeradorImg,
       colors: [

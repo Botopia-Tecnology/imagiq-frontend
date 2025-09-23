2111bd2bd1d74b21b722750933205a57754d70f3
andresfeliper3 <andresfeliper.2001@gmail.com>
2025-09-22 21:01:47 -0500

    Filtros configuardos para tablets

--- src/app/productos/dispositivos-moviles/constants/tabletsConstants.ts
+++ src/app/productos/dispositivos-moviles/constants/tabletsConstants.ts
@@ -46,15 +46,15 @@ export const tabletCategories: Category[] = [
 // Configuración de filtros específica para tabletas
 export const tabletFilters: FilterConfig = {
   serie: ["Galaxy Tab S", "Galaxy Tab A", "Galaxy Tab Active"],
-  pantalla: ['8"', '10.1"', '10.4"', '11"', '12.4"'],
+  //pantalla: ['8"', '10.1"', '10.4"', '11"', '12.4"'],
   almacenamiento: ["32GB", "64GB", "128GB", "256GB", "512GB"],
   conectividad: ["Wi-Fi", "LTE", "5G"],
   caracteristicas: [
     "S Pen",
     "Carga rápida",
-    "Resistente al agua",
+    // "Resistente al agua",
     "Carga inalámbrica",
-    "Dual SIM",
+    // "Dual SIM",
   ],
   rangoPrecio: [
     { label: "Menos de $500.000", min: 0, max: 500000 },
@@ -62,5 +62,6 @@ export const tabletFilters: FilterConfig = {
     { label: "$1.000.000 - $2.000.000", min: 1000000, max: 2000000 },
     { label: "Más de $2.000.000", min: 2000000, max: Infinity },
   ],
-  uso: ["Productividad", "Gaming", "Educación", "Entretenimiento"],
+  color: ["Negro", "Blanco", "Azul", "Rosa", "Verde", "Morado", "Dorado", "Plateado"],
+  //uso: ["Productividad", "Gaming", "Educación", "Entretenimiento"],
 };

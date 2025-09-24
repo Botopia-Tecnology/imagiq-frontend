ee07b32819b60f21c3756ee5b95f2526788c2158
Santiago Aristizabal <aristizabalsantiago482@gmail.com>
2025-09-24 09:34:54 -0500

    feat: conditionally display free shipping message in cart components

--- src/app/carrito/Step2.tsx
+++ src/app/carrito/Step2.tsx
@@ -850,9 +850,11 @@ export default function Step2({
                   <span className="font-bold">0</span>
                 </span>
               </div>
-              <div className="text-sm text-green-600">
-                tienes envío gratis en esta compra
-              </div>
+              {cartProducts.length > 0 && (
+                <div className="text-xs text-green-600">
+                  tienes envío gratis en esta compra
+                </div>
+              )}
             </div>
             <div className="flex justify-between text-lg font-bold mt-2">
               <span>Total</span>
